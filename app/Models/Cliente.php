496523d4078bb91ef08a70fe910c0acb140f5030
z-Nava <62721391+z-Nava@users.noreply.github.com>
2023-05-11 13:08:17 -0600

    Modelo cliente

---
+++ app/Models/Cliente.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    use HasFactory;
+    protected $table = 'cliente';
+
+    protected $fillable = [
+        'apellidos',
+        'nombre',
+        'documento',
+        'direccion',
+        'telefono',
+        'email'
+    ];
+
+}
